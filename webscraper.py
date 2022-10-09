d0d76e32b7ec73893ecbd8ccf0b44dca97dddc9d
Tian <59985851+TunaFi@users.noreply.github.com>
2022-10-09 18:18:53 -0400

    Story 2.1 – Data Scraping – Query list of available Form 10-K(s) matching criteria

--- webscraper.py
+++ webscraper.py
@@ -2,25 +2,23 @@ import requests
 import pandas as pd 
 from bs4 import BeautifulSoup
 
-# base URL for the SEC EDGAR browser
-endpoint = r"https://www.sec.gov/cgi-bin/browse-edgar"
+class WebScraper:
+    def make_URL(ciks):
+        # base URL for the SEC EDGAR browser
+        endpoint = r"https://www.sec.gov/cgi-bin/browse-edgar"
 
-# make comma separated string of inputs into a list of CIK's
-CIK = input()
-CIKList = CIK.split(",")
+        # define our parameters dictionary
+        for i in ciks:
+            param_dict = {'action':'getcompany',
+                        'CIK':i,
+                        'type':'10-k',
+                        'owner':'exclude',
+                        'output':''}
 
-# define our parameters dictionary
-for i in CIKList:
-    param_dict = {'action':'getcompany',
-                'CIK':i,
-                'type':'10-k',
-                'owner':'exclude',
-                'output':''}
+        # request the url, and then parse the response
+            response = requests.get(url = endpoint, params = param_dict)
+            soup = BeautifulSoup(response.content, 'html.parser')
 
-# request the url, and then parse the response
-    response = requests.get(url = endpoint, params = param_dict)
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-# print urls containing all 10-K filings for each company
-    print(i,":")
-    print(response.url)
+        # print urls containing all 10-K filings for each company
+            print(i,":")
+            print(response.url)
