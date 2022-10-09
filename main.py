e3d22ca0e9f6c552a629b391f4b48ebe4a839f2b
noahgarman <=>
2022-10-09 19:36:52 -0400

    Story 3.3 – Text Processing – Calculate term frequency

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import export_page as ep
 import search_page as sp
+from text_parser import Parser
+
+parser = Parser()
 
 home_page = sp.SearchPage()
 home_page.make_window()
@@ -7,4 +10,6 @@ home_page.make_window()
 
 end_page = ep.ExportPage()
 end_page.make_window()
+
+parser.demo_parser()
 
