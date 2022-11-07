fe8a708120d0b395ef4b2335c5cca9bd9886041b
Tian <59985851+TunaFi@users.noreply.github.com>
2022-11-06 22:51:58 -0500

    Story 2.3 - Data Scraping - Restricting queries based on filing year

--- webscraper.py
+++ webscraper.py
@@ -3,7 +3,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 class WebScraper:
-    def company_URL(cik, head):
+    def company_URL(self, cik, head):
         endpoint = r"https://www.sec.gov/cgi-bin/browse-edgar"
     
         param_dict = {'action':'getcompany',
@@ -16,7 +16,7 @@ class WebScraper:
         soup = BeautifulSoup(response.content, 'html.parser')
         return soup
     
-    def yearly_filings(soup, head):
+    def yearly_filings(self, soup, head, start, end):
         doc_table = soup.find_all('table', class_='tableFile2')
         base_url_sec = r"https://www.sec.gov"
         ten_k_soup = []
@@ -24,7 +24,13 @@ class WebScraper:
             cols = row.find_all('td')
             
             if len(cols) != 0:                  
-                filing_date = cols[3].text.strip()
+                filing_date = int(cols[3].text.strip()[0:4])
+
+                if filing_date > end:
+                    continue
+                elif filing_date < start:
+                    break 
+
                 filing_doc_href = cols[1].find('a', {'href':True, 'id':'documentsbutton'})       
 
                 if filing_doc_href != None:
@@ -54,13 +60,14 @@ class WebScraper:
                 print(ten_k_doc_link)
         return ten_k_soup
 
-    if __name__ == "__main__":
-        print("Enter cik:")
-        cik = input().split(",")
-        print("Enter your school email:")
-        email = input()
-        head = {'User-Agent': 'Georgia Southern University AdminContact@{email}'}
-        soup = company_URL(cik, head)
-        for x in yearly_filings(soup, head):
-            print(x.text)
+if __name__ == "__main__":
+    print("Enter cik:")
+    test = WebScraper()
+    cik = input().split(",")
+    print("Enter your school email:")
+    email = input()
+    head = {'User-Agent': 'Georgia Southern University AdminContact@{email}'}
+    soup = test.company_URL(cik, head)
+    for x in test.yearly_filings(soup, head, 2017, 2020):
+        continue
             
